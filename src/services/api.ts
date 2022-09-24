93f338a69701e3c728191ee72e53764907cbf47e
Wesley Belonci <wesbelonci@gmail.com>
2022-09-24 18:29:50 -0400

    updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,12 +4,12 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("@blazebull:token");
+// api.interceptors.request.use((config) => {
+//   const accessToken = localStorage.getItem("@blazebull:token");
 
-  api.defaults.headers.common = { Authorization: `bearer ${accessToken}` };
+//   api.defaults.headers.common = { Authorization: `bearer ${accessToken}` };
 
-  return config;
-});
+//   return config;
+// });
 
 export default api;
